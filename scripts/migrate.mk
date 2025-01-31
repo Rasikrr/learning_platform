8a1052a7d06e2d0465f067efabdcf1c5c9fb87d1
Rasikrr <rs.t.95@mail.ru>
2025-01-31 22:32:18 +0500

    add goose install

--- scripts/migrate.mk
+++ scripts/migrate.mk
@@ -3,12 +3,15 @@
 
 
 migrations_up:
+	go install github.com/pressly/goose/v3/cmd/goose@latest
 	goose -dir migrations postgres "$(POSTGRES_DSN)" up
 
 migrations_down:
+	go install github.com/pressly/goose/v3/cmd/goose@latest
 	goose -dir migrations postgres "$(POSTGRES_DSN)" down
 
 create_migration:
+	go install github.com/pressly/goose/v3/cmd/goose@latest
 	goose -dir "./migrations" create $(NAME) sql
 
 print_dsn:
